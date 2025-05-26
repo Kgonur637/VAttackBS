2b2f34adaf75ed1b50a84ee6d1ddba941a768dbc
Kgonur637 <kairavgonur@gmail.com>
2025-05-25 22:16:40 -0700

    Files Added

---
+++ brawlvoice.py
@@ -0,0 +1,47 @@
+import sys
+from ppadb.client import Client as AdbClient
+
+
+
+# Connect to ADB
+client = AdbClient(host="127.0.0.1", port=5037)
+device = client.devices()[0]
+
+# Read command-line argument
+#if len(sys.argv) < 2:
+    #print("No command provided.")
+    #sys.exit(1)
+
+#command = sys.argv[1]
+#command = input()
+# Map commands to coordinates
+
+#left, right, up, down, uleft, urignt, dleft, dright, shoot, super
+tap_map = {
+    'l': (358,804, 300, 804),
+    'r': (358,804, 408, 804),
+    'u':(358,804, 358, 756),
+    'd':(358,804, 358, 855),
+    'ul':(358,804,300,756),
+    'ur':(358,804,408,756),
+    'dl':(358,804,300,855),
+    'dr':(358,804,408,855),
+    'a':(1955,655),
+    's': (1642,784),
+    'ps': (1642,784,1642,850)
+
+
+}
+while True:
+    command = input()
+    if command in tap_map:
+        try:
+            x1, y1, x2, y2= tap_map[command]
+            print("swipe")
+            device.input_swipe(x1,y1,x2,y2,2000)
+        except:
+            x1, y1 = tap_map[command]
+            print("tap")
+            device.input_tap(x1, y1)
+    else:
+        print(f"Unknown command: {command}")
